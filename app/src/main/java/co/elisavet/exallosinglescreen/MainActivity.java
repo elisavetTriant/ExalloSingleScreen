beb1c89742f5209c6cf7da63458c83809bb5dddf
Elisavet Triantafyllopoulou <elitriant@gmail.com>
2018-02-26 12:52:12 +0200

    Updated MainActivity.java
    Updated method openWebPage so if intent is not resolved to inform user
    with a Toast.

--- app/src/main/java/co/elisavet/exallosinglescreen/MainActivity.java
+++ app/src/main/java/co/elisavet/exallosinglescreen/MainActivity.java
@@ -5,6 +5,7 @@ import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -40,6 +41,9 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
         if (intent.resolveActivity(getPackageManager()) != null) {
             startActivity(intent);
+        } else {
+            Toast.makeText(getApplicationContext(), "This button opens a Web Page and you don't seem to have a browser installed!", Toast.LENGTH_LONG).show();
         }
     }
+
 }
